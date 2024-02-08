6f278614f351960e3c2e72dd9ac7a41bf8ffc655
Alex Bowen <mab2098@g.rit.edu>
2024-02-08 16:09:58 -0500

    add public and private subnets

--- main.tf
+++ main.tf
@@ -31,3 +31,18 @@ variable "private_subnet_cidr_blocks" {
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 }
+
+resource "aws_subnet" "public" {
+  count                  = length(var.public_subnet_cidr_blocks)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = var.public_subnet_cidr_blocks[count.index]
+  availability_zone      = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count                  = length(var.private_subnet_cidr_blocks)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = var.private_subnet_cidr_blocks[count.index]
+  availability_zone      = element(data.aws_availability_zones.available.names, count.index)
+}
