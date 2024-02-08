c38c8b5f025ace59eb3691871674f5ee4a642dda
Alex Bowen <mab2098@g.rit.edu>
2024-02-08 16:07:32 -0500

    added varriables for public and private

--- main.tf
+++ main.tf
@@ -13,11 +13,17 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for public subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for private subnets"
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 }
